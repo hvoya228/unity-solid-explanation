2ab787d7accd94c57e9613a2e2015efeb2f7138c
chugaister228 <bogucukij@gmail.com>
2023-06-21 15:46:01 +0300

    s update

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Health : MonoBehaviour
 {
     private int health = 10;
 
